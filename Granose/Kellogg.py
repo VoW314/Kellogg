5c63b0e382c6fc2317cecbbb1fc2b5493ed4b8bd
SB <44678673+TST314@users.noreply.github.com>
2020-09-10 14:16:38 -0400

    Update Kellogg.py

--- Granose/Kellogg.py
+++ Granose/Kellogg.py
@@ -1,2 +1,7 @@
+'''
+Granose is the first flake cereal which was created by Kellogg.
+Of course there were versions before but this will be the first working/deployable version!
+'''
 import pynput
 import os, sys
+
